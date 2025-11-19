b69c53b125e60dfc30528cf80a8dd1a1f36ee13e
YannG75 <shadowyann@hotmail.com>
2025-11-19 16:45:50 +0100

    Add GitHub and LinkedIn icons to assets; update `BlocContact.vue` layout for improved styling and consistency.

--- app/components/BlocContact.vue
+++ app/components/BlocContact.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from 'vue'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import {ref, onMounted, onBeforeUnmount} from 'vue'
+import {gsap} from 'gsap'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -10,7 +10,7 @@ defineProps({
   hided: Boolean
 })
 
-const emit = defineEmits(['update:hovered','update:hided'])
+const emit = defineEmits(['update:hovered', 'update:hided'])
 
 // Refs pour les paragraphes du motto
 const mottoLine1 = ref<HTMLParagraphElement | null>(null)
@@ -23,21 +23,21 @@ onMounted(() => {
   lines.forEach((line) => {
     if (line) {
       gsap.fromTo(line,
-        {
-          backgroundPosition: '100% 0%'
-        },
-        {
-          backgroundPosition: '0% 0%',
-          duration: 1.5,
-          ease: 'power2.out',
-          scrollTrigger: {
-            trigger: line,
-            start: 'top 80%',
-            end: 'top 40%',
-            scrub: 1,
-            toggleActions: 'play none none reverse'
+          {
+            backgroundPosition: '100% 0%'
+          },
+          {
+            backgroundPosition: '0% 0%',
+            duration: 1.5,
+            ease: 'power2.out',
+            scrollTrigger: {
+              trigger: line,
+              start: 'top 80%',
+              end: 'top 40%',
+              scrub: 1,
+              toggleActions: 'play none none reverse'
+            }
           }
-        }
       )
     }
   })
@@ -52,28 +52,50 @@ onBeforeUnmount(() => {
   <section id="contact" class="w-full min-h-screen 2xl:min-h-[70vh] flex flex-col items-center justify-center">
     <div class="w-full px-4 sm:px-6 md:w-[70%] flex flex-col items-center justify-center mb-20">
       <h2 class="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-400 mb-10">My motto</h2>
-      <p ref="mottoLine1" class="text-gradient-scroll font-bold text-2xl sm:text-3xl md:text-5xl leading-snug w-full sm:w-[90%] text-center">
+      <p
+          ref="mottoLine1"
+          class="text-gradient-scroll font-bold text-2xl sm:text-3xl md:text-5xl leading-snug w-full sm:w-[90%] text-center">
         I'm Doing what I know
       </p>
-      <p ref="mottoLine2" class="text-gradient-scroll font-bold text-2xl sm:text-3xl md:text-5xl leading-snug w-full sm:w-[90%] text-center">
+      <p
+          ref="mottoLine2"
+          class="text-gradient-scroll font-bold text-2xl sm:text-3xl md:text-5xl leading-snug w-full sm:w-[90%] text-center">
         and
       </p>
-      <p ref="mottoLine3" class="text-gradient-scroll font-bold text-2xl sm:text-3xl md:text-5xl leading-snug w-full sm:w-[90%] text-center">
+      <p
+          ref="mottoLine3"
+          class="text-gradient-scroll font-bold text-2xl sm:text-3xl md:text-5xl leading-snug w-full sm:w-[90%] text-center">
         I'm learning what I don't know.
       </p>
     </div>
-    <h2 class="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-400 mb-10 text-center px-4">Feel free to reach out to me</h2>
-    <div class="w-full px-4 sm:px-6 md:w-[70%] flex flex-col sm:flex-row items-center justify-center gap-6 sm:gap-10 md:gap-20">
+    <h2 class="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-400 mb-10 text-center px-4">Feel free to reach out
+      to me</h2>
+    <div
+        class="w-full px-4 sm:px-6 md:w-[70%] flex flex-col sm:flex-row items-center justify-center gap-6 sm:gap-10 md:gap-20">
 
-      <article class="flex flex-col items-center justify-center w-fit relative group" @mouseover="emit('update:hided', true)" @mouseleave="emit('update:hided', false)">
+      <article
+          class="flex flex-col items-center justify-center w-fit relative group"
+          @mouseover="emit('update:hided', true)" @mouseleave="emit('update:hided', false)">
         <span class="z-1 text-bold text-gradient-hover text-base sm:text-lg">Email</span>
         <a href="mailto:yannalxr@gmail.com" class="z-1 text-gradient-hover text-sm sm:text-base">yannalxr@gmail.com</a>
       </article>
-      <article class="relative flex flex-col items-center justify-center w-fit group" @mouseover="emit('update:hided', true)" @mouseleave="emit('update:hided', false)">
+      <article
+          class="relative flex flex-col items-center justify-center w-fit group"
+          @mouseover="emit('update:hided', true)" @mouseleave="emit('update:hided', false)">
         <span class="text-bold z-1 text-gradient-hover text-base sm:text-lg">Phone</span>
         <a href="tel:+33645693436" class="z-1 text-gradient-hover text-sm sm:text-base">+33 6 45 69 34 36</a>
       </article>
     </div>
+    <article class="relative flex items-center justify-center w-full gap-6 group mt-10 mb-10">
+      <a
+          class="bg-[#E5E5E5] text-[#545454] hover:bg-primary px-2 py-1 rounded-md shadow-xl"
+          href="https://www.linkedin.com/in/yann-grillon/"
+          target="_blank"
+          @mouseover="emit('update:hided', true)"
+          @mouseleave="emit('update:hided', false)">
+        <NuxtImg src="/assets/icons/linkedin.svg" alt="github logo" class="w-5 h-5 sm:w-6 sm:h-6"/>
+      </a>
+    </article>
   </section>
 </template>
 
--- app/components/BlocWorks.vue
+++ app/components/BlocWorks.vue
@@ -110,12 +110,7 @@ const works = ref<Work[]>([
           <div v-if="clicked == work.name" class="flex gap-4 sm:gap-8 md:gap-5 z-10 justify-center items-center">
               <a class="bg-[#E5E5E5] font-[sora] text-[#545454] text-sm sm:text-base px-4 sm:px-5 py-2 rounded-md shadow-xl " :href="work.link" target="_blank">check it out</a>
               <a v-if="work.github" class="bg-[#E5E5E5] text-[#545454] px-4 sm:px-5 py-1 rounded-md shadow-xl" :href="work.github" target="_blank">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 sm:w-8 sm:h-8" viewBox="0 0 24 24">
-                  <!-- Icon from Material Design Icons by Pictogrammers - https://github.com/Templarian/MaterialDesign/blob/master/LICENSE -->
-                  <path
-                      fill="currentColor"
-                      d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"/>
-                </svg>
+                <NuxtImg src="/assets/icons/github.svg" alt="github logo" class="w-5 h-5 sm:w-6 sm:h-6" />
               </a>
           </div>
 
