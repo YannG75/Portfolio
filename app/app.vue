96a046555b721784a518a1f1dcd910e7291c9c28
YannG75 <shadowyann@hotmail.com>
2025-11-19 12:02:07 +0100

    Improve mousemove event handling with `requestAnimationFrame` for smoother cursor animations; adjust formatting for consistency.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import gsap from 'gsap'
-import { ref, onMounted, onBeforeUnmount } from 'vue'
+import {ref, onMounted, onBeforeUnmount} from 'vue'
 import BlocNavigation from '~/components/BlocNavigation.vue'
 import BlocHero from '~/components/BlocHero.vue'
 import BlocAboutMe from '~/components/BlocAboutMe.vue'
@@ -32,14 +32,15 @@ onMounted(() => {
 
   window.addEventListener("mousemove", (e) => {
     if (!mouseCursor.value) return
-    if (hovered.value) {
-      xTo(e.clientX - 20)
-      yTo(e.clientY - 20)
-    }
-    else {
-      xTo(e.clientX - 10)
-      yTo(e.clientY - 7)
-    }
+    requestAnimationFrame(() => {
+      if (hovered.value) {
+        xTo(e.clientX - 20)
+        yTo(e.clientY - 20)
+      } else {
+        xTo(e.clientX - 10)
+        yTo(e.clientY - 7)
+      }
+    })
   })
 
   const options: IntersectionObserverInit = {
@@ -74,68 +75,70 @@ onBeforeUnmount(() => {
     observer.disconnect()
     observer = null
   }
-  window.removeEventListener("mousemove", () => {})
+  window.removeEventListener("mousemove", () => {
+  })
 })
 </script>
 
 <template>
   <!-- Lucioles en arrière-plan -->
-  <FirefliesBackground />
+  <FirefliesBackground/>
 
   <main
-    class="relative w-full flex flex-col"
+      class="relative w-full flex flex-col"
   >
     <div
-      ref="mouseCursor"
-      :class="[hovered ? 'w-8 h-8 sm:w-10 sm:h-10 cursor-hover' : '', hided ? '!w-0 !h-0 ' : '']"
-      class="fixed will-change-transform pointer-events-none opacity-90 w-5 h-5 sm:w-7 sm:h-7 z-20 cursor-transition bg-primary duration-150 ease rounded-full border-primary"
+        ref="mouseCursor"
+        :class="[hovered ? 'w-8 h-8 sm:w-10 sm:h-10 cursor-hover' : '', hided ? '!w-0 !h-0 ' : '']"
+        class="fixed will-change-transform pointer-events-none opacity-90 w-5 h-5 sm:w-7 sm:h-7 z-20 cursor-transition bg-primary duration-150 ease rounded-full border-primary"
     />
 
     <!-- On passe activeSection à la navigation -->
     <BlocNavigation
-      :hided="hided"
-      :active-section="activeSection"
-      @update:hided="hided = $event"
+        :hided="hided"
+        :active-section="activeSection"
+        @update:hided="hided = $event"
     />
 
-    <NuxtImg src="assets/images/profil.png" alt="Hero" class="hidden md:block absolute top-0 left-0 md:w-[50vw] md:h-[100vh] object-cover z-0" />
+    <NuxtImg src="assets/images/profil.png" alt="Hero"
+             class="hidden md:block absolute top-0 left-0 md:w-[50vw] md:h-[100vh] object-cover z-0"/>
 
     <!-- BlocHero -->
     <section id="hero" ref="heroRef">
 
       <BlocHero
-        :hovered="hovered"
-        @update:hovered="hovered = $event"
+          :hovered="hovered"
+          @update:hovered="hovered = $event"
       />
     </section>
 
     <!-- About -->
     <section id="about" ref="aboutRef">
       <BlocAboutMe
-        :hovered="hovered"
-        :hided="hided"
-        @update:hovered="hovered = $event"
-        @update:hided="hided = $event"
+          :hovered="hovered"
+          :hided="hided"
+          @update:hovered="hovered = $event"
+          @update:hided="hided = $event"
       />
     </section>
 
     <!-- BlocWorks -->
     <section id="works" ref="worksRef">
       <BlocWorks
-        :hovered="hovered"
-        :hided="hided"
-        @update:hovered="hovered = $event"
-        @update:hided="hided = $event"
+          :hovered="hovered"
+          :hided="hided"
+          @update:hovered="hovered = $event"
+          @update:hided="hided = $event"
       />
     </section>
 
     <!-- BlocContact -->
     <section id="contact" ref="contactRef">
       <BlocContact
-        :hovered="hovered"
-        :hided="hided"
-        @update:hovered="hovered = $event"
-        @update:hided="hided = $event"
+          :hovered="hovered"
+          :hided="hided"
+          @update:hovered="hovered = $event"
+          @update:hided="hided = $event"
       />
     </section>
   </main>
